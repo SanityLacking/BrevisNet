e2bb01d06aaa47b84ac4f856dd53d26e422762fe
cailen robertson <cailen.robertson@griffithuni.edu.au>
2020-12-14 21:22:55 +1000

    work on branchy training

--- main.py
+++ main.py
@@ -37,12 +37,12 @@ def fullprint(*args, **kwargs):
 
 
 def newBranch(prevLayer, outputs):
-        print(prevLayer)
-        print(prevLayer.shape)
-        branchLayer = layers.Dense(124, activation="relu")(prevLayer)
-        branchLayer = layers.Dense(64, activation="relu")(branchLayer)
-        outputs.append(layers.Dense(10, name="branch_output_{}".format(len(outputs)+1))(branchLayer))
-        return outputs
+    print(prevLayer)
+    print(prevLayer.shape)
+    branchLayer = layers.Dense(124, activation="relu",name=tf.compat.v1.get_default_graph().unique_name("branch124"))(prevLayer)
+    branchLayer = layers.Dense(64, activation="relu",name=tf.compat.v1.get_default_graph().unique_name("branch64"))(branchLayer)
+    outputs.append(layers.Dense(10, name=tf.compat.v1.get_default_graph().unique_name("branch_output"))(branchLayer))
+    return outputs
 
 def calcEntropy(y_hat):
         #entropy is the sum of y * log(y) for all possible labels.
@@ -59,17 +59,97 @@ def saveModel(model,name,overwrite = True, folder ="models", fileFormat = "hdf5"
     from datetime import datetime
     import os
     now = datetime.now() # current date and time
-    
+    stringName =""
     if not os.path.exists(folder):
         try:
             os.mkdir(folder)
         except FileExistsError:
             pass
     try:
-        model.save("{}{}_{}.{}".format(folder+"\\",name,now.strftime("%y-%m-%d_%H%M%S"),fileFormat))
+        stringName = "{}{}_{}.{}".format(folder+"\\",name,now.strftime("%y-%m-%d_%H%M%S"),fileFormat)
+        model.save(stringName)
     except OSError:
         pass
-    return True
+    return stringName
+
+
+#https://github.com/keras-team/keras/issues/341
+def reset_model_weights(model):
+    for layer in model.layers:
+        if isinstance(layer, tf.keras.Model): #if you're using a model as a layer
+            reset_weights(layer) #apply function recursively
+            continue
+
+        #where are the initializers?
+        if hasattr(layer, 'cell'):
+            init_container = layer.cell
+        else:
+            init_container = layer
+
+        for key, initializer in init_container.__dict__.items():
+            if "initializer" not in key: #is this item an initializer?
+                  continue #if no, skip it
+
+            # find the corresponding variable, like the kernel or the bias
+            if key == 'recurrent_initializer': #special case check
+                var = getattr(init_container, 'recurrent_kernel')
+            else:
+                var = getattr(init_container, key.replace("_initializer", ""))
+
+            var.assign(initializer(var.shape, var.dtype))
+            #use the initializer
+
+def reset_layer_weights(layer):
+    """ reset the weights for a specific layer.
+    """
+
+    #where are the initializers?
+    if hasattr(layer, 'cell'):
+        init_container = layer.cell
+    else:
+        init_container = layer
+
+    for key, initializer in init_container.__dict__.items():
+        if "initializer" not in key: #is this item an initializer?
+                continue #if no, skip it
+
+        # find the corresponding variable, like the kernel or the bias
+        if key == 'recurrent_initializer': #special case check
+            var = getattr(init_container, 'recurrent_kernel')
+        else:
+            var = getattr(init_container, key.replace("_initializer", ""))
+
+        var.assign(initializer(var.shape, var.dtype))
+        #use the initializer
+
+def reset_branch_weights(model):
+    for layer in model.layers:
+        if isinstance(layer, tf.keras.Model): #if you're using a model as a layer
+            reset_branch_weights(layer) #apply function recursively
+            continue
+        if "branch" in layer.name:
+             #where are the initializers?
+            if hasattr(layer, 'cell'):
+                init_container = layer.cell
+            else:
+                init_container = layer
+
+            for key, initializer in init_container.__dict__.items():
+                if "initializer" not in key: #is this item an initializer?
+                    continue #if no, skip it
+
+                # find the corresponding variable, like the kernel or the bias
+                if key == 'recurrent_initializer': #special case check
+                    var = getattr(init_container, 'recurrent_kernel')
+                else:
+                    var = getattr(init_container, key.replace("_initializer", ""))
+
+                var.assign(initializer(var.shape, var.dtype))
+                #use the initializer
+        else: 
+            pass
+
+
 
 class BranchyNet:
     def mainBranch(self):
@@ -171,7 +251,13 @@ class BranchyNet:
             config = KerasModel.get_config()   
             return KerasModel
 
-    def trainMnist(self, model, epocs = 2,save = False):
+
+
+
+    def trainModel(self, model, epocs = 2,save = False):
+        """
+        Train the model that is passed through. This function works for both single and multiple branch models.
+        """
         logs = []
         (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
         num_outputs = len(model.outputs) # the number of output layers for the purpose of providing labels
@@ -185,25 +271,18 @@ class BranchyNet:
             history = model.fit(x_train, list(itertools.repeat(y_train,num_outputs)), batch_size=64, epochs=1, validation_split=0.2)
             print(history)
             test_scores = model.evaluate(x_test, list(itertools.repeat(y_test,num_outputs)), verbose=2)
-            # print(test_scores)
             print("overall loss: {}".format(test_scores[0]))
-            # results.append("Output {}: Test loss: {}, Test accuracy {}".format(0, test_scores[1], test_scores[2]))
             if num_outputs > 1:
                 for i in range(num_outputs):
                     print("Output {}: Test loss: {}, Test accuracy {}".format(i, test_scores[i+1], test_scores[i+1+num_outputs]))
                     results.append("Output {}: Test loss: {}, Test accuracy {}".format(i, test_scores[i+1], test_scores[i+1+num_outputs]))
             else:
-                # results.append("Output {}: Test loss: {}, Test accuracy {}".format(0, test_scores[1], test_scores[2]))
                 print("Test loss:", test_scores[0])
                 print("Test accuracy:", test_scores[1])
             logs.append(results)
         # fullprint(model.predict(x_test[:1]))
         if save:
-            # with CustomObjectScope({'GlorotUniform': glorot_uniform()}):
-            model.save("models/mnistNormal2.hdf5")
-            new_model = keras.models.load_model('models/mnistNormal2.hdf5')
-            # new_model = load_model("models/mnistNormal2.hdf5")
-            new_model.summary()
+            saveModel(model,"mnist_trained")
 
         results = model.predict(x_test[:1])
         fullprint(results)
@@ -212,6 +291,65 @@ class BranchyNet:
         # print(logs)
         return model
 
+    def trainModelTransfer(self, model, resetBranches = False, epocs = 2,save = False):
+        """
+        Train the model that is passed using transfer learning. This function expects a model with trained main branches and untrained (or randomized) side branches.
+        """
+        logs = []
+        (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+        num_outputs = len(model.outputs) # the number of output layers for the purpose of providing labels
+        # print(np.repeat([y_train],1))
+        x_train = x_train.reshape(60000, 784).astype("float32") / 255
+        x_test = x_test.reshape(10000, 784).astype("float32") / 255
+
+        #Freeze main branch layers
+        #how to iterate through layers and find main branch ones?
+        #simple fix for now: all branch nodes get branch in name.
+        if resetBranches: 
+            reset_branch_weights(model)
+
+        for i in range(len(model.layers)):
+            print(model.layers[i].name)
+            # print(model.layers[i].initial_weights)
+
+            if "branch" in model.layers[i].name:
+                print("branchLayer")
+                model.layers[i].trainable = True
+            else: 
+                model.layers[i].trainable = False
+            # for j in range(len(model.layers[i].inbound_nodes)):
+            #     print(dir(model.layers[i].inbound_nodes[j]))
+            #     print("inboundNode: " + model.layers[i].inbound_nodes[j].name)
+            #     print("outboundNode: " + model.layers[i].outbound_nodes[j].name)
+
+        model.compile(loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True), optimizer=keras.optimizers.Adam(),metrics=["accuracy"])
+
+        for j in range(epocs):
+            results = [j]           
+            history = model.fit(x_train, list(itertools.repeat(y_train,num_outputs)), batch_size=64, epochs=1, validation_split=0.2)
+            print(history)
+            test_scores = model.evaluate(x_test, list(itertools.repeat(y_test,num_outputs)), verbose=2)
+            print("overall loss: {}".format(test_scores[0]))
+            if num_outputs > 1:
+                for i in range(num_outputs):
+                    print("Output {}: Test loss: {}, Test accuracy {}".format(i, test_scores[i+1], test_scores[i+1+num_outputs]))
+                    results.append("Output {}: Test loss: {}, Test accuracy {}".format(i, test_scores[i+1], test_scores[i+1+num_outputs]))
+            else:
+                print("Test loss:", test_scores[0])
+                print("Test accuracy:", test_scores[1])
+            logs.append(results)
+        # fullprint(model.predict(x_test[:1]))
+        if save:
+            saveModel(model,"mnist_transfer_trained")
+
+        # results = model.predict(x_test[:1])
+        # fullprint(results)
+        # S = entropy(results)
+        # print(S)
+
+
+        # print(logs)
+        return model
 
 
 from scipy.special import (comb, chndtr, entr, rel_entr, xlogy, ive)
@@ -276,40 +414,19 @@ if __name__ == "__main__":
         branchy = BranchyNet()
         # x = branchy.mainBranch()
         # x = branchy.mnistNormal()
-        x = branchy.mnistBranchy()
-        # inputs = keras.Input(shape=(784,))
-        # x = layers.Flatten(input_shape=(28,28))(inputs)
-        # x = layers.Dense(512, activation="relu")(x)
-        # x= layers.Dropout(0.2)(x)
-        # #exit 2
-        # x = layers.Dense(512, activation="relu")(x)
-        # x= layers.Dropout(0.2)(x)
-        # #exit 3
-        # x = layers.Dense(512, activation="relu")(x)
-        # x= layers.Dropout(0.2)(x)
-        # #exit 4
-        # x = layers.Dense(512, activation="relu")(x)
-        # x= layers.Dropout(0.2)(x)
-        # #exit 5
-        # x = layers.Dense(512, activation="relu")(x)
-        # x= layers.Dropout(0.2)(x)
-        # #exit 1 The main branch exit is refered to as "exit 1" or "main exit" to avoid confusion when adding addtional exits
-        # output1 = layers.Dense(10, name="output1")(x)
-        # # outputs.append(output1)
-        # model = keras.Model(inputs=inputs, outputs=output1, name="mnist_model")
 
-        # model.save("models/mnistNormal2.hdf5")
+        # x = branchy.mnistBranchy()
+        # x.summary()
+        # x.save("models/mnistbranchy.hdf5")
 
-        # new_model = keras.models.load_model('models/mnistNormal2.hdf5')
-        # x = new_model
-        # x = branchy.mnistExample()
-        # x = branchy.loadModel("models/mnistNormal2.hdf5")
+        # x = keras.models.load_model('models/mnistbranchy.hdf5')
+        
+        # x = branchy.mnistBranchy()
+        # x = branchy.trainModel(x, 1,save = False)
+        x = branchy.loadModel("models/mnist2_transfer_20-12-14_181326.hdf5")
+        x = branchy.trainModelTransfer(x, 1,save = False)
 
-        x = branchy.trainMnist(x, 1,save = False)
         # x.save("models/mnistNormal2_trained.hdf5")
-        saveModel(x,"mnist2_trained")
-
-        new_model = keras.models.load_model('models/mnistNormal2_trained.hdf5')
-        new_model.summary()
+        saveModel(x,"mnist2_transfer_trained")
 
     pass
