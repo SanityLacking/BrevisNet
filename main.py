c9d1140c519339b03487a7a646486558d9adefbd
cailenR <cailen01@gmail.com>
2020-12-15 15:23:53 +1000

    added generic addbranches function

--- main.py
+++ main.py
@@ -37,8 +37,6 @@ def fullprint(*args, **kwargs):
 
 
 def newBranch(prevLayer, outputs):
-    print(prevLayer)
-    print(prevLayer.shape)
     branchLayer = layers.Dense(124, activation="relu",name=tf.compat.v1.get_default_graph().unique_name("branch124"))(prevLayer)
     branchLayer = layers.Dense(64, activation="relu",name=tf.compat.v1.get_default_graph().unique_name("branch64"))(branchLayer)
     branchLayer = layers.Dense(10, name=tf.compat.v1.get_default_graph().unique_name("branch_output"))(branchLayer)
@@ -55,6 +53,57 @@ def calcEntropy(y_hat):
             print(entropy)
 
         return results
+
+from scipy.special import (comb, chndtr, entr, rel_entr, xlogy, ive)
+def entropy(pk, qk=None, base=None):
+    #taken from branchynet github
+    """Calculate the entropy of a distribution for given probability values.
+
+    If only probabilities `pk` are given, the entropy is calculated as
+    ``S = -sum(pk * log(pk), axis=0)``.
+
+    If `qk` is not None, then compute the Kullback-Leibler divergence
+    ``S = sum(pk * log(pk / qk), axis=0)``.
+
+    This routine will normalize `pk` and `qk` if they don't sum to 1.
+
+    Parameters
+    ----------
+    pk : sequence
+        Defines the (discrete) distribution. ``pk[i]`` is the (possibly
+        unnormalized) probability of event ``i``.
+    qk : sequence, optional
+        Sequence against which the relative entropy is computed. Should be in
+        the same format as `pk`.
+    base : float, optional
+        The logarithmic base to use, defaults to ``e`` (natural logarithm).
+
+    Returns
+    -------
+    S : float
+        The calculated entropy.
+
+    """
+    pk = np.asarray(pk)
+    print(pk)
+    print(1.0*pk)
+    print(np.sum(pk,axis=0))
+    pk = 1.0*pk / np.sum(pk, axis=0)
+    print(pk)
+    if qk is None:
+        vec = entr(pk)
+    else:
+        qk = np.asarray(qk)
+        if len(qk) != len(pk):
+            raise ValueError("qk and pk must have same length.")
+        qk = 1.0*qk / np.sum(qk, axis=0)
+        vec = rel_entr(pk, qk)
+    print(vec)
+    S = np.sum(vec, axis=0)
+    if base is not None:
+        S /= math.log(base)
+    return S
+
 
 
 def saveModel(model,name,overwrite = True, includeDate= True, folder ="models", fileFormat = "tf"):
@@ -271,6 +320,61 @@ class BranchyNet:
         model = keras.Model(inputs=inputs, outputs=outputs, name="mnist_model_branched")
         return model
 
+    def addBranches(self,model, identifier =[""], customBranch = []):
+        """ add branches to the provided model, aka modifying an existing model to include branches.
+            identifier: takes a list of names of layers to branch on is blank, branches will be added to all layers except the input and final layer. Can be a list of layer numbers, following the numbering format of model.layers[]
+            If identifier is not blank, a branch will be added to each layer with identifier in its name. (identifier = "dense", all dense layers will be branched.)
+            Warning! individual layers are defined according to how TF defines them. this means that for layers that would be normally grouped, they will be treated as individual layers (conv2d, pooling, flatten, etc)
+            customBranch: optional function that can be passed to provide a custom branch to be inserted. Check "newBranch" function for default shape of branches and how to build custom branching function. Can be provided as a list and each branch will iterate through provided customBranches, repeating last the last branch until function completes
+                
+
+        """
+
+        outputs = []
+        outputs.append(model.outputs) #get model outputs that already exist 
+
+        if type(identifier) != list:
+            identifier = [identifier]
+        
+
+        if type(customBranch) != list:
+            customBranch = [customBranch]
+        if len(customBranch) == 0:
+            customBranch = [newBranch]
+
+        if len(identifier) > 0:
+            print(">0")
+            if type(identifier[0]) == int:
+                print("int")
+                for i in identifier: 
+                    print(model.layers[i].name)
+                    try:
+                        outputs = customBranch[min(i, len(customBranch))-1](model.layers[i].output,outputs)
+                        # outputs = newBranch(model.layers[i].output,outputs)
+                    except:
+                        pass
+            else:
+                print("abc")
+                for i in range(len(model.layers)):
+                    print(model.layers[i].name)
+                    if any(id in model.layers[i].name for id in identifier):
+                        outputs = customBranch[min(i, len(customBranch))-1](model.layers[i].output,outputs)
+                        # outputs = newBranch(model.layers[i].output,outputs)
+        else: #if identifier is blank or empty
+            print("nothing")
+            for i in range(1-len(model.layers)-1):
+                print(model.layers[i].name)
+                # if "dense" in model.layers[i].name:
+                # outputs = newBranch(model.layers[i].output,outputs)
+                outputs = customBranch[max(i, len(customBranch))](model.layers[i].output,outputs)
+            # for j in range(len(model.layers[i].inbound_nodes)):
+            #     print(dir(model.layers[i].inbound_nodes[j]))
+            #     print("inboundNode: " + model.layers[i].inbound_nodes[j].name)
+            #     print("outboundNode: " + model.layers[i].outbound_nodes[j].name)
+
+        model = keras.Model(inputs=model.inputs, outputs=outputs, name="{}_branched".format(model.name))
+        return model
+
 
     def fullprint(*args, **kwargs):
         from pprint import pprint
@@ -341,17 +445,16 @@ class BranchyNet:
         x_test = x_test.reshape(10000, 784).astype("float32") / 255
 
 
-        print("before reset:")
-        model.compile(loss="sparse_categorical_crossentropy", optimizer=keras.optimizers.Adam(),metrics=["accuracy"])
-        test_scores = model.evaluate(x_test, list(itertools.repeat(y_test,num_outputs)), verbose=2)
-        printTestScores(test_scores,num_outputs)
+        # print("before reset:")
+        # model.compile(loss="sparse_categorical_crossentropy", optimizer=keras.optimizers.Adam(),metrics=["accuracy"])
+        # test_scores = model.evaluate(x_test, list(itertools.repeat(y_test,num_outputs)), verbose=2)
+        # printTestScores(test_scores,num_outputs)
 
         #Freeze main branch layers
         #how to iterate through layers and find main branch ones?
         #simple fix for now: all branch nodes get branch in name.
         if resetBranches: 
             reset_branch_weights(model)
-
         for i in range(len(model.layers)):
             print(model.layers[i].name)
             # print(model.layers[i].initial_weights)
@@ -376,6 +479,7 @@ class BranchyNet:
 
 
         for j in range(epocs):
+            print("epoc: {}".format(j))
             results = [j]           
             history = model.fit(x_train, list(itertools.repeat(y_train,num_outputs)), batch_size=64, epochs=1, validation_split=0.2)
             print(history)
@@ -403,55 +507,29 @@ class BranchyNet:
         return model
 
 
-from scipy.special import (comb, chndtr, entr, rel_entr, xlogy, ive)
-def entropy(pk, qk=None, base=None):
-    #taken from branchynet github
-    """Calculate the entropy of a distribution for given probability values.
+        ###### RUN MODEL SHORTCUTS ######
 
-    If only probabilities `pk` are given, the entropy is calculated as
-    ``S = -sum(pk * log(pk), axis=0)``.
+    def Run_mnistNormal(self, numEpocs = 2):
+        """ load a mnist model, add branches to it and train using transfer learning function
+        """
+        x = branchy.mnistNormal()
+        x = branchy.trainModel(x,epocs = numEpocs,save = False)
+        return x
 
-    If `qk` is not None, then compute the Kullback-Leibler divergence
-    ``S = sum(pk * log(pk / qk), axis=0)``.
 
-    This routine will normalize `pk` and `qk` if they don't sum to 1.
+    def Run_mnistTransfer(self, numEpocs = 2):
+        """ load a mnist model, add branches to it and train using transfer learning function
+        """
+        x = tf.keras.models.load_model("models/mnist2_trained_.tf")
+        x.summary()
+        x = branchy.addBranches(x,["dropout"],)
+        x.summary()
+        # x = branchy.trainModelTransfer(x,epocs = numEpocs, save = False)
+        return x
 
-    Parameters
-    ----------
-    pk : sequence
-        Defines the (discrete) distribution. ``pk[i]`` is the (possibly
-        unnormalized) probability of event ``i``.
-    qk : sequence, optional
-        Sequence against which the relative entropy is computed. Should be in
-        the same format as `pk`.
-    base : float, optional
-        The logarithmic base to use, defaults to ``e`` (natural logarithm).
 
-    Returns
-    -------
-    S : float
-        The calculated entropy.
 
-    """
-    pk = np.asarray(pk)
-    print(pk)
-    print(1.0*pk)
-    print(np.sum(pk,axis=0))
-    pk = 1.0*pk / np.sum(pk, axis=0)
-    print(pk)
-    if qk is None:
-        vec = entr(pk)
-    else:
-        qk = np.asarray(qk)
-        if len(qk) != len(pk):
-            raise ValueError("qk and pk must have same length.")
-        qk = 1.0*qk / np.sum(qk, axis=0)
-        vec = rel_entr(pk, qk)
-    print(vec)
-    S = np.sum(vec, axis=0)
-    if base is not None:
-        S /= math.log(base)
-    return S
+
 
 
 if __name__ == "__main__":
@@ -463,20 +541,11 @@ if __name__ == "__main__":
 
     if True:        
         branchy = BranchyNet()
-        # x = branchy.mainBranch()
+        # x = branchy.Run_mnistNormal(1)
+        x = branchy.Run_mnistTransfer(1)
 
         # x = branchy.mnistBranchy()
-
-
-        # x = branchy.mnistNormal()
-
-        # x = branchy.mnistBranchy()
-        # x = branchy.trainModel(x, 1,save = False)
-        # saveModel(x,name="mnist2_trained",includeDate=False,fileFormat="tf")
-        x = tf.keras.models.load_model("models/mnist2_trained_.tf")
-
-        x = branchy.mnistAddBranches(x)
-        x = branchy.trainModelTransfer(x, 1,True, save = False)
+        
 
         # x = branchy.loadModel("models/mnist_trained_20-12-15_112434.hdf5")
         # x = tf.keras.models.load_model("models/mnist2_transfer_trained_.tf")
@@ -485,3 +554,4 @@ if __name__ == "__main__":
         # saveModel(x,"mnist2_transfer_trained_final",includeDate=False)
 
     pass
+
