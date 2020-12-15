b3d778bf6e4bf48128dd1f987fe6be411a14394a
cailenR <cailen01@gmail.com>
2020-12-15 16:27:24 +1000

    finished custombranch function, moved some functions to utils.py

--- main.py
+++ main.py
@@ -16,205 +16,8 @@ import os
 #os.environ["PATH"] += os.pathsep + "C:\Program Files\Graphviz\bin"
 #from tensorflow.keras.utils import plot_model
 
+from utils import *
 
-
-#Visualize Model
-def visualize_model(model,name=""):
-    # tf.keras.utils.plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
-    if name == "":
-        name = "model_plot.png"
-    else: 
-        name = name + ".png"
-    #plot_model(model, to_file=name, show_shapes=True, show_layer_names=True)
-
-def fullprint(*args, **kwargs):
-        from pprint import pprint
-        import numpy
-        opt = numpy.get_printoptions()
-        numpy.set_printoptions(threshold=numpy.inf)
-        pprint(*args, **kwargs)
-        numpy.set_printoptions(**opt)
-
-
-def newBranch(prevLayer, outputs):
-    branchLayer = layers.Dense(124, activation="relu",name=tf.compat.v1.get_default_graph().unique_name("branch124"))(prevLayer)
-    branchLayer = layers.Dense(64, activation="relu",name=tf.compat.v1.get_default_graph().unique_name("branch64"))(branchLayer)
-    branchLayer = layers.Dense(10, name=tf.compat.v1.get_default_graph().unique_name("branch_output"))(branchLayer)
-    outputs.append(layers.Softmax(name=tf.compat.v1.get_default_graph().unique_name("branch_softmax"))(branchLayer))
-
-    return outputs
-
-def calcEntropy(y_hat):
-        #entropy is the sum of y * log(y) for all possible labels.
-        results =[]
-        entropy = 0
-        for i in range(len(y_hat)):
-            entropy += y_hat[i] * math.log(y_hat(i))
-            print(entropy)
-
-        return results
-
-from scipy.special import (comb, chndtr, entr, rel_entr, xlogy, ive)
-def entropy(pk, qk=None, base=None):
-    #taken from branchynet github
-    """Calculate the entropy of a distribution for given probability values.
-
-    If only probabilities `pk` are given, the entropy is calculated as
-    ``S = -sum(pk * log(pk), axis=0)``.
-
-    If `qk` is not None, then compute the Kullback-Leibler divergence
-    ``S = sum(pk * log(pk / qk), axis=0)``.
-
-    This routine will normalize `pk` and `qk` if they don't sum to 1.
-
-    Parameters
-    ----------
-    pk : sequence
-        Defines the (discrete) distribution. ``pk[i]`` is the (possibly
-        unnormalized) probability of event ``i``.
-    qk : sequence, optional
-        Sequence against which the relative entropy is computed. Should be in
-        the same format as `pk`.
-    base : float, optional
-        The logarithmic base to use, defaults to ``e`` (natural logarithm).
-
-    Returns
-    -------
-    S : float
-        The calculated entropy.
-
-    """
-    pk = np.asarray(pk)
-    print(pk)
-    print(1.0*pk)
-    print(np.sum(pk,axis=0))
-    pk = 1.0*pk / np.sum(pk, axis=0)
-    print(pk)
-    if qk is None:
-        vec = entr(pk)
-    else:
-        qk = np.asarray(qk)
-        if len(qk) != len(pk):
-            raise ValueError("qk and pk must have same length.")
-        qk = 1.0*qk / np.sum(qk, axis=0)
-        vec = rel_entr(pk, qk)
-    print(vec)
-    S = np.sum(vec, axis=0)
-    if base is not None:
-        S /= math.log(base)
-    return S
-
-
-
-def saveModel(model,name,overwrite = True, includeDate= True, folder ="models", fileFormat = "tf"):
-    from datetime import datetime
-    import os
-    now = datetime.now() # current date and time
-    stringName =""
-    date =""
-    if not os.path.exists(folder):
-        try:
-            os.mkdir(folder)
-        except FileExistsError:
-            pass
-    try:
-        if includeDate:
-            date =now.strftime("%y-%m-%d_%H%M%S")
-
-        stringName = "{}{}_{}.{}".format(folder+"\\",name,date,fileFormat)
-        model.save(stringName, save_format="fileFormat")
-        print("saved Model:{}".format(stringName))
-    except OSError:
-        pass
-
-    return stringName
-
-def printTestScores(test_scores,num_outputs):
-    print("overall loss: {}".format(test_scores[0]))
-    if num_outputs > 1:
-        for i in range(num_outputs):
-            print("Output {}: Test loss: {}, Test accuracy {}".format(i, test_scores[i+1], test_scores[i+1+num_outputs]))
-    else:
-        print("Test loss:", test_scores[0])
-        print("Test accuracy:", test_scores[1])
-
-
-#https://github.com/keras-team/keras/issues/341
-def reset_model_weights(model):
-    for layer in model.layers:
-        if isinstance(layer, tf.keras.Model): #if you're using a model as a layer
-            reset_weights(layer) #apply function recursively
-            continue
-
-        #where are the initializers?
-        if hasattr(layer, 'cell'):
-            init_container = layer.cell
-        else:
-            init_container = layer
-
-        for key, initializer in init_container.__dict__.items():
-            if "initializer" not in key: #is this item an initializer?
-                  continue #if no, skip it
-
-            # find the corresponding variable, like the kernel or the bias
-            if key == 'recurrent_initializer': #special case check
-                var = getattr(init_container, 'recurrent_kernel')
-            else:
-                var = getattr(init_container, key.replace("_initializer", ""))
-
-            var.assign(initializer(var.shape, var.dtype))
-            #use the initializer
-
-def reset_layer_weights(layer):
-    """ reset the weights for a specific layer.
-    """
-
-    #where are the initializers?
-    if hasattr(layer, 'cell'):
-        init_container = layer.cell
-    else:
-        init_container = layer
-
-    for key, initializer in init_container.__dict__.items():
-        if "initializer" not in key: #is this item an initializer?
-                continue #if no, skip it
-
-        # find the corresponding variable, like the kernel or the bias
-        if key == 'recurrent_initializer': #special case check
-            var = getattr(init_container, 'recurrent_kernel')
-        else:
-            var = getattr(init_container, key.replace("_initializer", ""))
-
-        var.assign(initializer(var.shape, var.dtype))
-        #use the initializer
-
-def reset_branch_weights(model):
-    for layer in model.layers:
-        if isinstance(layer, tf.keras.Model): #if you're using a model as a layer
-            reset_branch_weights(layer) #apply function recursively
-            continue
-        if "branch" in layer.name:
-            print("reseting weights for {}".format(layer.name))
-             #where are the initializers?
-            if hasattr(layer, 'cell'):
-                init_container = layer.cell
-            else:
-                init_container = layer
-
-            for key, initializer in init_container.__dict__.items():
-                if "initializer" not in key: #is this item an initializer?
-                    continue #if no, skip it
-
-                # find the corresponding variable, like the kernel or the bias
-                if key == 'recurrent_initializer': #special case check
-                    var = getattr(init_container, 'recurrent_kernel')
-                else:
-                    var = getattr(init_container, key.replace("_initializer", ""))
-
-                var.assign(initializer(var.shape, var.dtype))
-                #use the initializer
-        else: 
-            pass
 
 
 
@@ -229,6 +32,7 @@ class BranchyNet:
 ])
         return model
 
+        
     
 
     def mnistNormal(self):
@@ -326,10 +130,7 @@ class BranchyNet:
             If identifier is not blank, a branch will be added to each layer with identifier in its name. (identifier = "dense", all dense layers will be branched.)
             Warning! individual layers are defined according to how TF defines them. this means that for layers that would be normally grouped, they will be treated as individual layers (conv2d, pooling, flatten, etc)
             customBranch: optional function that can be passed to provide a custom branch to be inserted. Check "newBranch" function for default shape of branches and how to build custom branching function. Can be provided as a list and each branch will iterate through provided customBranches, repeating last the last branch until function completes
-                
-
         """
-
         outputs = []
         outputs.append(model.outputs) #get model outputs that already exist 
 
@@ -341,7 +142,9 @@ class BranchyNet:
             customBranch = [customBranch]
         if len(customBranch) == 0:
             customBranch = [newBranch]
-
+        
+        branches = 0
+        print(customBranch)
         if len(identifier) > 0:
             print(">0")
             if type(identifier[0]) == int:
@@ -349,7 +152,8 @@ class BranchyNet:
                 for i in identifier: 
                     print(model.layers[i].name)
                     try:
-                        outputs = customBranch[min(i, len(customBranch))-1](model.layers[i].output,outputs)
+                        outputs = customBranch[min(branches, len(customBranch))-1](model.layers[i].output,outputs)
+                        branches=branches+1
                         # outputs = newBranch(model.layers[i].output,outputs)
                     except:
                         pass
@@ -358,7 +162,10 @@ class BranchyNet:
                 for i in range(len(model.layers)):
                     print(model.layers[i].name)
                     if any(id in model.layers[i].name for id in identifier):
-                        outputs = customBranch[min(i, len(customBranch))-1](model.layers[i].output,outputs)
+                        # print(customBranch[min(i, len(customBranch))-1])
+                        # print(min(i, len(customBranch))-1)
+                        outputs = customBranch[min(branches, len(customBranch))-1](model.layers[i].output,outputs)
+                        branches=branches+1
                         # outputs = newBranch(model.layers[i].output,outputs)
         else: #if identifier is blank or empty
             print("nothing")
@@ -366,7 +173,8 @@ class BranchyNet:
                 print(model.layers[i].name)
                 # if "dense" in model.layers[i].name:
                 # outputs = newBranch(model.layers[i].output,outputs)
-                outputs = customBranch[max(i, len(customBranch))](model.layers[i].output,outputs)
+                outputs = customBranch[min(branches, len(customBranch))-1](model.layers[i].output,outputs)
+                branches=branches+1
             # for j in range(len(model.layers[i].inbound_nodes)):
             #     print(dir(model.layers[i].inbound_nodes[j]))
             #     print("inboundNode: " + model.layers[i].inbound_nodes[j].name)
@@ -509,6 +317,10 @@ class BranchyNet:
 
         ###### RUN MODEL SHORTCUTS ######
 
+  
+
+
+
     def Run_mnistNormal(self, numEpocs = 2):
         """ load a mnist model, add branches to it and train using transfer learning function
         """
@@ -522,12 +334,20 @@ class BranchyNet:
         """
         x = tf.keras.models.load_model("models/mnist2_trained_.tf")
         x.summary()
-        x = branchy.addBranches(x,["dropout"],)
+        x = branchy.addBranches(x,["dropout"],newBranch)
         x.summary()
         # x = branchy.trainModelTransfer(x,epocs = numEpocs, save = False)
         return x
 
 
+
+def newBranchCustom(prevLayer, outputs=[]):
+    """ example of a custom branching layer, used as a drop in replacement of "newBranch"
+    """                 
+    branchLayer = layers.Dense(10, name=tf.compat.v1.get_default_graph().unique_name("branch_output"))(prevLayer)
+    outputs.append(layers.Softmax(name=tf.compat.v1.get_default_graph().unique_name("branch_softmax"))(branchLayer))
+
+    return outputs
 
 
 
---
+++ utils.py
@@ -0,0 +1,223 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import load_model
+import itertools
+
+# from keras.models import load_model
+# from keras.utils import CustomObjectScope
+# from keras.initializers import glorot_uniform
+
+import math
+import pydot
+import os
+#os.environ["PATH"] += os.pathsep + "C:\Program Files\Graphviz\bin"
+#from tensorflow.keras.utils import plot_model
+
+
+
+#Visualize Model
+def visualize_model(model,name=""):
+    # tf.keras.utils.plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+    if name == "":
+        name = "model_plot.png"
+    else: 
+        name = name + ".png"
+    #plot_model(model, to_file=name, show_shapes=True, show_layer_names=True)
+
+def fullprint(*args, **kwargs):
+        from pprint import pprint
+        import numpy
+        opt = numpy.get_printoptions()
+        numpy.set_printoptions(threshold=numpy.inf)
+        pprint(*args, **kwargs)
+        numpy.set_printoptions(**opt)
+
+
+
+def calcEntropy(y_hat):
+        #entropy is the sum of y * log(y) for all possible labels.
+        results =[]
+        entropy = 0
+        for i in range(len(y_hat)):
+            entropy += y_hat[i] * math.log(y_hat(i))
+            print(entropy)
+
+        return results
+
+from scipy.special import (comb, chndtr, entr, rel_entr, xlogy, ive)
+def entropy(pk, qk=None, base=None):
+    #taken from branchynet github
+    """Calculate the entropy of a distribution for given probability values.
+
+    If only probabilities `pk` are given, the entropy is calculated as
+    ``S = -sum(pk * log(pk), axis=0)``.
+
+    If `qk` is not None, then compute the Kullback-Leibler divergence
+    ``S = sum(pk * log(pk / qk), axis=0)``.
+
+    This routine will normalize `pk` and `qk` if they don't sum to 1.
+
+    Parameters
+    ----------
+    pk : sequence
+        Defines the (discrete) distribution. ``pk[i]`` is the (possibly
+        unnormalized) probability of event ``i``.
+    qk : sequence, optional
+        Sequence against which the relative entropy is computed. Should be in
+        the same format as `pk`.
+    base : float, optional
+        The logarithmic base to use, defaults to ``e`` (natural logarithm).
+
+    Returns
+    -------
+    S : float
+        The calculated entropy.
+
+    """
+    pk = np.asarray(pk)
+    print(pk)
+    print(1.0*pk)
+    print(np.sum(pk,axis=0))
+    pk = 1.0*pk / np.sum(pk, axis=0)
+    print(pk)
+    if qk is None:
+        vec = entr(pk)
+    else:
+        qk = np.asarray(qk)
+        if len(qk) != len(pk):
+            raise ValueError("qk and pk must have same length.")
+        qk = 1.0*qk / np.sum(qk, axis=0)
+        vec = rel_entr(pk, qk)
+    print(vec)
+    S = np.sum(vec, axis=0)
+    if base is not None:
+        S /= math.log(base)
+    return S
+
+
+
+def saveModel(model,name,overwrite = True, includeDate= True, folder ="models", fileFormat = "tf"):
+    from datetime import datetime
+    import os
+    now = datetime.now() # current date and time
+    stringName =""
+    date =""
+    if not os.path.exists(folder):
+        try:
+            os.mkdir(folder)
+        except FileExistsError:
+            pass
+    try:
+        if includeDate:
+            date =now.strftime("%y-%m-%d_%H%M%S")
+
+        stringName = "{}{}_{}.{}".format(folder+"\\",name,date,fileFormat)
+        model.save(stringName, save_format="fileFormat")
+        print("saved Model:{}".format(stringName))
+    except OSError:
+        pass
+
+    return stringName
+
+def printTestScores(test_scores,num_outputs):
+    print("overall loss: {}".format(test_scores[0]))
+    if num_outputs > 1:
+        for i in range(num_outputs):
+            print("Output {}: Test loss: {}, Test accuracy {}".format(i, test_scores[i+1], test_scores[i+1+num_outputs]))
+    else:
+        print("Test loss:", test_scores[0])
+        print("Test accuracy:", test_scores[1])
+
+
+#https://github.com/keras-team/keras/issues/341
+def reset_model_weights(model):
+    for layer in model.layers:
+        if isinstance(layer, tf.keras.Model): #if you're using a model as a layer
+            reset_weights(layer) #apply function recursively
+            continue
+
+        #where are the initializers?
+        if hasattr(layer, 'cell'):
+            init_container = layer.cell
+        else:
+            init_container = layer
+
+        for key, initializer in init_container.__dict__.items():
+            if "initializer" not in key: #is this item an initializer?
+                  continue #if no, skip it
+
+            # find the corresponding variable, like the kernel or the bias
+            if key == 'recurrent_initializer': #special case check
+                var = getattr(init_container, 'recurrent_kernel')
+            else:
+                var = getattr(init_container, key.replace("_initializer", ""))
+
+            var.assign(initializer(var.shape, var.dtype))
+            #use the initializer
+
+def reset_layer_weights(layer):
+    """ reset the weights for a specific layer.
+    """
+
+    #where are the initializers?
+    if hasattr(layer, 'cell'):
+        init_container = layer.cell
+    else:
+        init_container = layer
+
+    for key, initializer in init_container.__dict__.items():
+        if "initializer" not in key: #is this item an initializer?
+                continue #if no, skip it
+
+        # find the corresponding variable, like the kernel or the bias
+        if key == 'recurrent_initializer': #special case check
+            var = getattr(init_container, 'recurrent_kernel')
+        else:
+            var = getattr(init_container, key.replace("_initializer", ""))
+
+        var.assign(initializer(var.shape, var.dtype))
+        #use the initializer
+
+def reset_branch_weights(model):
+    for layer in model.layers:
+        if isinstance(layer, tf.keras.Model): #if you're using a model as a layer
+            reset_branch_weights(layer) #apply function recursively
+            continue
+        if "branch" in layer.name:
+            print("reseting weights for {}".format(layer.name))
+             #where are the initializers?
+            if hasattr(layer, 'cell'):
+                init_container = layer.cell
+            else:
+                init_container = layer
+
+            for key, initializer in init_container.__dict__.items():
+                if "initializer" not in key: #is this item an initializer?
+                    continue #if no, skip it
+
+                # find the corresponding variable, like the kernel or the bias
+                if key == 'recurrent_initializer': #special case check
+                    var = getattr(init_container, 'recurrent_kernel')
+                else:
+                    var = getattr(init_container, key.replace("_initializer", ""))
+
+                var.assign(initializer(var.shape, var.dtype))
+                #use the initializer
+        else: 
+            pass
+
+
+def newBranch(prevLayer, outputs=[]):
+    """ Add a new branch to a model connecting at the output of prevLayer. 
+        outputs is a list of all outputs for the model, if you don't add this to the model.compile step, its as if the branch wasn't added at all. 
+        returns list of outputs for the model.
+        NOTE: use the substring "branch" in all names for branch nodes. this is used as an identifier of the branching layers as opposed to the main branch layers for training
+    """ 
+    branchLayer = layers.Dense(124, activation="relu",name=tf.compat.v1.get_default_graph().unique_name("branch124"))(prevLayer)
+    branchLayer = layers.Dense(64, activation="relu",name=tf.compat.v1.get_default_graph().unique_name("branch64"))(branchLayer)
+    branchLayer = layers.Dense(10, name=tf.compat.v1.get_default_graph().unique_name("branch_output"))(branchLayer)
+    outputs.append(layers.Softmax(name=tf.compat.v1.get_default_graph().unique_name("branch_softmax"))(branchLayer))
+
+    return outputs
